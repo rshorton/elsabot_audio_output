951de5c31f93dcccff45bcc1b1abc69639b9743c
Scott Horton <horton.rscott@gmail.com>
2026-03-17 21:51:31 -0500

    Revise logging

--- elsabot_audio_output/tts_provider_http_piper.py
+++ elsabot_audio_output/tts_provider_http_piper.py
@@ -6,32 +6,32 @@ import requests
 from .tts_provider import TTSProvider
 
 class TTSProviderHTTPPiper(TTSProvider):
-  def __init__(self, logger):
-      TTSProvider.__init__(self, logger, 'local_piper')
+    def __init__(self, logger):
+        TTSProvider.__init__(self, logger, 'local_piper')
       
-      self.tts_url = os.getenv('TTS_PIPER_URL', 'http://localhost:5000') 
-      self.tts_timeout = os.getenv('TTS_PIPER_TIMEOUT', 10)
+        self.tts_url = os.getenv('TTS_PIPER_URL', 'http://localhost:5000') 
+        self.tts_timeout = os.getenv('TTS_PIPER_TIMEOUT', 10)
   
-  def request_tts(self, text):
-      req_data = text
-      json_data = json.dumps(req_data)
+    def request_tts(self, text):
+        req_data = text
 
-      try:
-          response = requests.post(self.tts_url, json=json_data, timeout=self.tts_timeout)
-          self.logger.debug(f"TTS Piper response status: {response.status_code}, size {len(response.content)}")
-          if response.status_code == 200:
-              tts_io_buf = io.BytesIO()
-              tts_io_buf.name = 'tts.wav'
-              tts_io_buf.write(response.content)
-              return tts_io_buf
+        try:
+            self.logger.debug(f"TTS Piper making request: {req_data}")
+            response = requests.post(self.tts_url, data=req_data, timeout=self.tts_timeout)
+            self.logger.debug(f"TTS Piper response status: {response.status_code}, size {len(response.content)}")
+            if response.status_code == 200:
+                tts_io_buf = io.BytesIO()
+                tts_io_buf.name = 'tts.wav'
+                tts_io_buf.write(response.content)
+                return tts_io_buf
 
-      except requests.exceptions.ConnectTimeout:
-          self.logger.error(f'TTS conversion failed, connection timed out')
+        except requests.exceptions.ConnectTimeout:
+            self.logger.error(f'TTS Piper conversion failed, connection timed out')
 
-      except requests.exceptions.ReadTimeout:
-          self.logger.error(f'TTS conversion failed, read timed out')
+        except requests.exceptions.ReadTimeout:
+            self.logger.error(f'TTS Piper conversion failed, read timed out')
 
-      except Exception as e:
-          self.logger.error(f'TTS conversion failed, ex: {e}')
+        except Exception as e:
+            self.logger.error(f'TTS Piper conversion failed, ex: {e}')
 
-      return None
+        return None
